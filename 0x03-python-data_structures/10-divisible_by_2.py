978cfb3f8bcca02604809c17979344b0174fcc62
Mcjayoap <mcjayoap>
2022-12-06 06:19:28 +0000

    find number divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            multiples.append(True)
+            return True
         else:
-            multiples.append(False)
+            return False
 
     return (multiples)
