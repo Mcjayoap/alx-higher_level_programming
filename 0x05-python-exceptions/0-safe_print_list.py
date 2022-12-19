90ecf2f3e2f945c3c278e21f355ba22ebdc6e34c
Mcjayoap <mcjayoap>
2022-12-19 22:44:43 +0000

    go ok

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 0
+    printed = 0
     for i in range(int(x)):
         try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
+    print()
+    return printed
+
