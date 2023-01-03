dc5d6c6716126d16b46d44b772585bf93ccd1951
Mcjayoap <mcjayoap>
2023-01-03 11:52:29 +0000

    .

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,6 @@ defines a rectangle by: (based on 0-rectangle.py)
 """
 class Rectangle:
     """Represent a rectangle."""
-
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
