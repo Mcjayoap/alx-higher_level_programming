0dde9bb19613f7ec0aac94d11a48d444c45a699a
Mcjayoap <mcjayoap>
2023-01-10 21:13:16 +0000

    fiz task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Defines an object attribute lookup"""
+"""Module with the method lookup"""
 
 def lookup(obj):
-    """ returns the list of available attributes and methods of an object"""
+    """Returns the attributes of an object"""
     return (dir(obj))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" Defines a class that inheruts from another class """
+""" Defines a class that inherits from another class """
 
+class list:
+    def __init__(self, MyList = []):
+        self.MyList = MyList
 
 class MyList(list):
     """ implement sorted printing of the built-in list class """
-
 
     def print_sorted(self):
         """ class MyList that inherits from list """
