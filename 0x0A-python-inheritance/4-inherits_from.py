c73d910fe77923f81086f5fde2d76400a523d7db
Mcjayoap <mcjayoap>
2023-01-11 07:01:07 +0000

    correction task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Module with methods inherits from """
+"""
+===================================
+module with method inherits_from
+===================================
+"""
 
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if the object is an instance 
-    of a class that inherited (directly or 
-    indirectly) from the specified class ;
-    otherwise False.
-    """
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
 
-    return False if type(obj) is a_class else isinstance(obj, class)
+    return False if type(obj) is a_class else isinstance(obj, a_class)
