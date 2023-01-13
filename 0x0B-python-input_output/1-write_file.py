d6e59e5176778baeab832f5350e545531014b200
Mcjayoap <mcjayoap>
2023-01-13 17:13:35 +0000

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
+
+
+def write_file(my_first_file="", text=""):
     """writes a string to a text file (UTF8) and returns the number of characters"""
-    with open(filename, "w", encoding="UTF-8") as f:
+    with open("my_first_file.txt", "w", encoding="UTF-8") as f:
         return f.write(text)
