5760f1f606d111ffcd1ed5e73eb48002f203ca9b
Mcjayoap <mcjayoap>
2023-01-13 16:55:04 +0000

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,7 @@
 def read_file(filename=""):
     """function that reads a text file and prints it"""
 
+
     with open("my_file_0.txt", "r") as f:
         text = f.read()
         print(text, end="")
-
-    f.close()
