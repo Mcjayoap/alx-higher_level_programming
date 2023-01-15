12de9614b5847ac95ebc598c3e9148559776704a
Mcjayoap <mcjayoap>
2023-01-15 23:53:42 +0000

    .

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This class inherits from the base class"""
+from models.base import Base
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
